99d70ea87d39f90dbceaa653dc54cf988b9a4354
hzh-dxyl <13544968369@163.com>
2021-09-04 13:16:37 +0800

    删除了阿里云的key和secret

--- src/main/java/bean/Value.java
+++ src/main/java/bean/Value.java
@@ -10,6 +10,4 @@ public class Value {
     public static String defaultHeadPath= "images/default";
     public static String defaultGroupHead ="group.png";
 
-    public static final String Access_key="LTAI5tEUG7Jw2FvkSpSL3vxV";
-    public static final String Secret_key="DTtXJ97uOv974Z7y3A4FoF8d2mrRTM";
 }
