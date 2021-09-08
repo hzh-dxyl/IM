6d0af05dd99d2489b35c923fdd783ecc1a3e2e2a
hzh-dxyl <13544968369@163.com>
2021-09-08 13:26:35 +0800

    修改了本地图片存储地址

--- src/main/java/bean/Value.java
+++ src/main/java/bean/Value.java
@@ -6,7 +6,7 @@ public class Value {
             "bbe67369f092f6ecaf1637ade478e05d",
             "52dd4c14d6a7c8f5a1d74de954da7f4c",
             "b103d99e2ac9651f1e69650181178fc8"};
-    public static String imgLocalPath = "D:/课程/maven/IM/src/main/webapp/images/";
+    public static String imgLocalPath = "D:/课程/maven/IM/images/";//"D:/课程/maven/IM/src/main/webapp/images/";
     public static String defaultHeadPath= "images/default";
     public static String defaultGroupHead ="group.png";
 
